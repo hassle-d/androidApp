215f50f8ec6f800822686fadbb55403c886e053d
snguyenep <stephane.nguyen@epitech.eu>
2017-05-02 22:16:57 +0200

    Modified build.gradle SDK version

--- AndroidApp/app/src/main/java/com/androidapp/app/AppConfig.java
+++ AndroidApp/app/src/main/java/com/androidapp/app/AppConfig.java
@@ -5,4 +5,11 @@ package com.androidapp.app;
  */
 
 public class AppConfig {
+
+    // OutputMedialFile request codes
+    public static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+
+    // directory name to store captured images
+    public static final String IMAGE_DIRECTORY_NAME = "AndroidApp";
 }
